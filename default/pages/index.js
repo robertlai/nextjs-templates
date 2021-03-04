75b59c87c48c5920573abee191d850e062768288
Robert Lai <rcylai@hotmail.com>
2021-03-03 22:32:34 -0800

    Add default template

---
+++ default/pages/index.js
@@ -0,0 +1,9 @@
+import styles from 'styles/pages/Home.module.scss';
+
+const Home = () => (
+    <div className={styles.container}>
+        <h1>Hello world!</h1>
+    </div>
+);
+
+export default Home;
